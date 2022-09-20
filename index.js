aa59d438a0155272ece0a61e273079b61d68ba1a
Nicolas Rodriguez <nircandsim@gmail.com>
2022-09-20 18:43:32 -0500

    Modelos, controladores y rutas de pais

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const app = express();
 
 dbConnection();
 
-app.use(express.static('Public'));
+//app.use(express.static('Public'));
+
+app.use(express.json());
+
+app.use('/api/pais',require('./Routes/pais'))
 
 app.listen(process.env.Port, () => {
     console.log(`Servidor corriendo en el puerto ${process.env.Port}`);
 });
+
+module.exports = app
