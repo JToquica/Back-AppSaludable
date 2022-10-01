de0c4808dc9284e49c55d6cdf65049e2b0abb58d
Nicolas Rodriguez <nircandsim@gmail.com>
2022-10-01 00:24:36 -0500

    Ruta y controlador para actulizar tipo recomendacion

--- Routes/tipoRecomendacion.js
+++ Routes/tipoRecomendacion.js
@@ -6,18 +6,30 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const { AdminRole } = require('../middlewares/validar-roles');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
-const {obtenerTipoRecomendacion, crearTipoRecomendacion} = require('../controllers/tipoRecomendacion');
+const { obtenerTipoRecomendacion, crearTipoRecomendacion, actulizarTipoRecomendacion } = require('../controllers/tipoRecomendacion');
 
 router.get('/', validarJWT, obtenerTipoRecomendacion);
 
 router.post('/create',
     [
-        check('nombre','El nombre del tipo recomendacion es obligatorio').not().isEmpty(),
+        check('nombre', 'El nombre del tipo recomendacion es obligatorio').not().isEmpty(),
     ],
     validarCampos,
     validarJWT,
     AdminRole,
     crearTipoRecomendacion
+);
+
+router.put(
+    '/update/:id',
+    [
+        check('nombre', 'El nombre del tipo recomendacion es obligatorio').not().isEmpty(),
+    ],
+    validarCampos,
+    validarJWT,
+    AdminRole,
+    actulizarTipoRecomendacion
+
 );
 
 module.exports = router;
--- controllers/tipoRecomendacion.js
+++ controllers/tipoRecomendacion.js
@@ -38,7 +38,37 @@ const crearTipoRecomendacion = async (req, resp = response) => {
     }
 }
 
+const actulizarTipoRecomendacion = async (req, resp = response) => {
+    const tipoRecomendacionId = req.params.id;
+
+    try {
+        const tipoRecomendacion = await TipoRecomendacion.findById(tipoRecomendacionId);
+
+        if(!tipoRecomendacion){
+            return resp.status(201).json({
+                ok: false,
+                msg: 'El id no corresponde a ningun tipo recomendacion',
+            });
+        }
+        const tipoRecomendacionActualizado = await TipoRecomendacion.findByIdAndUpdate(tipoRecomendacionId, req.body, {new: true});
+
+        return resp.status(201).json({
+            ok: true,
+            msg: 'Tipo recomendacion actualizada',
+            tipoRecomendacion: tipoRecomendacionActualizado
+        });
+
+    } catch (error) {
+        console.log(error);
+        return resp.status(400).json({
+            ok: false,
+            msg: 'Error al actualizar el tipo recomendacion',
+        });
+    }
+}
+
 module.exports = {
     obtenerTipoRecomendacion,
-    crearTipoRecomendacion
+    crearTipoRecomendacion,
+    actulizarTipoRecomendacion
 }
